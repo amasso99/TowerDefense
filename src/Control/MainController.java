1e36974ec4ece13702123ec9267d37966a32ba11
amasso99 <khouri.oussama@gmx.de>
2017-06-02 01:11:35 +0200

    general structure

---
+++ src/Control/MainController.java
@@ -0,0 +1,7 @@
+package Control;
+
+/**
+ * Created by Oussama on 02.06.2017.
+ */
+public class MainController {
+}
---
+++ src/Model/Example.java
@@ -0,0 +1,7 @@
+package Model;
+
+/**
+ * Created by Oussama on 02.06.2017.
+ */
+public class Example {
+}
