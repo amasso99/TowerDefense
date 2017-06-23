eba0e5220ad2a17e6466ec93279f71e0636a1038
a-gro <alexgrom1511@gmail.com>
2017-06-23 08:21:54 +0100

    settingscreen, bugfix in button

---
+++ src/Model/UI/Screen/SettingScreen.java
@@ -0,0 +1,41 @@
+package Model.UI.Screen;
+
+import Control.ScreenController;
+import Model.UI.ActionEvent;
+import View.Panel.DrawingPanel;
+import Model.UI.Button;
+import java.awt.Font;
+
+/**
+ * Created by Alex on 23.06.2017.
+ */
+public class SettingScreen extends DrawingPanel implements ActionEvent{
+
+    private Font f;
+    private ScreenController screenController;
+
+    public SettingScreen (ScreenController screenController){
+        this.screenController = screenController;
+
+        f = new Font("Arial", Font.PLAIN,32);
+        initAndAddObjects();
+    }
+
+    public void initAndAddObjects() {
+        int btnWidth = (int) (screenWidth*0.7);
+        int btnHeight = (int) (screenHeight*0.1);
+
+        Button btnBack = new Button("Back", (int) (screenWidth*0.5-btnWidth/2), (int) (screenHeight*0.7), btnWidth, btnHeight, "back", f);
+        btnBack.setActionListener(this);
+        addObject(btnBack);
+    }
+
+    @Override
+    public void doAction(String id) {
+        switch(id){
+            case("back"):
+                screenController.switchScreen(ScreenController.Type.MAIN_SCREEN);
+                break;
+        }
+    }
+}
