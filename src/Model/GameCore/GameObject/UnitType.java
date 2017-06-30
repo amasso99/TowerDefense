918d6931cb1bfc2b5784d2ffeda2b4f3ef2c2d02
amasso99 <khouri.oussama@gmx.de>
2017-06-30 02:26:22 +0200

    general structure

--- src/Model/GameCore/GameObject/UnitType.java
+++ src/Model/GameCore/GameObject/UnitType.java
@@ -4,12 +4,12 @@ package Model.GameCore.GameObject;
  * Created by 204g07 on 02.06.2017.
  */
 public enum UnitType { //attack,range,health,attackSpeed,speed,armor,ground,aimsGround,level,mana. level 0 = locked
-    KNIGHT(new Statistics(10,1,200,1.0,1.6,0,100)),
-    ARCHER(new Statistics(20,6,30,1.0,1.2,0,120)),
-    WIZARD(new Statistics(30,4,50,0.8,1.3,0,150)),
-    CAVALIER(new Statistics(50,2,300,1.2,1.5,0,600)),
-    DRAGON(new Statistics(40,3,500,1.1,1.1,0,500)),
-    GIANT(new Statistics(3,1,1000,0.6,1.4,0,300));
+    KNIGHT(new Statistics(10,200,200,1.0,1.6,0,100)),
+    ARCHER(new Statistics(20,600,30,1.0,1.2,0,120)),
+    WIZARD(new Statistics(30,400,50,0.8,1.3,0,150)),
+    CAVALIER(new Statistics(50,200,300,1.2,1.5,0,600)),
+    DRAGON(new Statistics(40,300,500,1.1,1.1,0,500)),
+    GIANT(new Statistics(3,100,1000,0.6,1.4,0,300));
 
     Statistics stats;
 
